fb241fab376c807e77e3b6fea14e7b69b37b4e8f
Sebastian <mail@sadrian.eu>
2024-06-19 22:52:50 +0200

    add Darkmode toggle

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">EniCa</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,6 +24,11 @@
           </li>
         </ul>
       </div>
+      <!-- Bootstrap 5 DarkMode Toggle -->
+      <div class="form-check form-switch">
+        <input class="form-check-input" type="checkbox" id="darkModeSwitch" checked>
+        <label class="form-check-label" for="darkModeSwitch">Dark Mode</label>
+      </div>
     </div>
   </nav>
   <main>
@@ -44,4 +49,31 @@ export default {
   }
 
 }
+
+// Bootstrap 5 DarkMode Toggle
+document.addEventListener('DOMContentLoaded', () => {
+  const htmlElement = document.documentElement;
+  const switchElement = document.getElementById('darkModeSwitch');
+
+  const currentTheme = localStorage.getItem('bsTheme') || 'dark';
+  htmlElement.setAttribute('data-bs-theme', currentTheme);
+  switchElement.checked = currentTheme === 'dark';
+
+  switchElement.addEventListener('change', function () {
+    if (this.checked) {
+      htmlElement.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('bsTheme', 'dark');
+    } else {
+      htmlElement.setAttribute('data-bs-theme', 'light');
+      localStorage.setItem('bsTheme', 'light');
+    }
+  });
+});
 </script>
+
+<style lang="sass">
+
+$enable-dark-mode: false !default
+
+</style>
+
